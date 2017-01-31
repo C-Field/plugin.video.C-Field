698305c942235c8f6504704257b281f2ca24a26e
C-Field <C-Field@users.noreply.github.com>
2017-01-31 10:14:39 +0000

    Update _Edit.py

--- _Edit.py
+++ _Edit.py
@@ -1,4 +1,4 @@
 import xbmcaddon
 
-MainBase = 'https://raw.githubusercontent.com/stuartdrew/plugin.video.C-Field/master/_home.txt'
+MainBase = 'https://raw.githubusercontent.com/C-Field/plugin.video.C-Field/master/_home1.txt'
 addon = xbmcaddon.Addon('plugin.video.C-Field')
