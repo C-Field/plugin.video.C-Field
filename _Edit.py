1ae9ebe2dcfcc0bae379e6e93e1ae9b49b3bbacb
rely-Field <rely-Field@users.noreply.github.com>
2017-02-01 17:47:11 +0000

    Update _Edit.py

--- _Edit.py
+++ _Edit.py
@@ -1,4 +1,4 @@
 import xbmcaddon
 
-MainBase = 'https://raw.githubusercontent.com/C-Field/plugin.video.C-Field/master/_home1.txt'
+MainBase = 'https://raw.githubusercontent.com/D-Field/plugin.video.C-Field/master/_home1.txt'
 addon = xbmcaddon.Addon('plugin.video.C-Field')
